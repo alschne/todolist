8a5d6e6f2d402b87f1c7ad82a0fecc22ad5420e6
Allie Roth <91703696+alschne@users.noreply.github.com>
2025-01-23 17:17:01 -0700

    initialize xcode project

---
+++ todolist/todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Allison Roth on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todolist/todolistTests/todolistTests.swift
@@ -0,0 +1,17 @@
+//
+//  todolistTests.swift
+//  todolistTests
+//
+//  Created by Allison Roth on 1/23/25.
+//
+
+import Testing
+@testable import todolist
+
+struct todolistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
